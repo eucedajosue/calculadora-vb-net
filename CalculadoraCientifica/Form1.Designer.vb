1ac91e3625dde64f794f0a2efbb84b0924a23327
Josue Euceda <josue.euceda@gmail.com>
2017-07-22 17:40:39 -0600

    Terminar Calculadora normal

--- CalculadoraCientifica/Form1.Designer.vb
+++ CalculadoraCientifica/Form1.Designer.vb
@@ -22,7 +22,7 @@ Partial Class CalculadoraForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ButtonNull = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
         Me.Button0 = New System.Windows.Forms.Button()
         Me.ButtonPunto = New System.Windows.Forms.Button()
         Me.ButtonMenos = New System.Windows.Forms.Button()
@@ -39,19 +39,22 @@ Partial Class CalculadoraForm
         Me.Button9 = New System.Windows.Forms.Button()
         Me.ButtonMultiplicar = New System.Windows.Forms.Button()
         Me.PantallaTxt = New System.Windows.Forms.TextBox()
+        Me.PantallaSecundariaTXT = New System.Windows.Forms.TextBox()
+        Me.ButtonIgual = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'ButtonNull
+        'ButtonClear
         '
-        Me.ButtonNull.Location = New System.Drawing.Point(12, 329)
-        Me.ButtonNull.Name = "ButtonNull"
-        Me.ButtonNull.Size = New System.Drawing.Size(80, 80)
-        Me.ButtonNull.TabIndex = 0
-        Me.ButtonNull.UseVisualStyleBackColor = True
+        Me.ButtonClear.Location = New System.Drawing.Point(17, 358)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(80, 80)
+        Me.ButtonClear.TabIndex = 0
+        Me.ButtonClear.Text = "C"
+        Me.ButtonClear.UseVisualStyleBackColor = True
         '
         'Button0
         '
-        Me.Button0.Location = New System.Drawing.Point(98, 329)
+        Me.Button0.Location = New System.Drawing.Point(103, 358)
         Me.Button0.Name = "Button0"
         Me.Button0.Size = New System.Drawing.Size(80, 80)
         Me.Button0.TabIndex = 0
@@ -60,7 +63,7 @@ Partial Class CalculadoraForm
         '
         'ButtonPunto
         '
-        Me.ButtonPunto.Location = New System.Drawing.Point(184, 329)
+        Me.ButtonPunto.Location = New System.Drawing.Point(189, 358)
         Me.ButtonPunto.Name = "ButtonPunto"
         Me.ButtonPunto.Size = New System.Drawing.Size(80, 80)
         Me.ButtonPunto.TabIndex = 0
@@ -69,16 +72,16 @@ Partial Class CalculadoraForm
         '
         'ButtonMenos
         '
-        Me.ButtonMenos.Location = New System.Drawing.Point(270, 329)
+        Me.ButtonMenos.Location = New System.Drawing.Point(275, 296)
         Me.ButtonMenos.Name = "ButtonMenos"
-        Me.ButtonMenos.Size = New System.Drawing.Size(80, 80)
+        Me.ButtonMenos.Size = New System.Drawing.Size(80, 60)
         Me.ButtonMenos.TabIndex = 0
         Me.ButtonMenos.Text = "-"
         Me.ButtonMenos.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 243)
+        Me.Button1.Location = New System.Drawing.Point(17, 272)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(80, 80)
         Me.Button1.TabIndex = 0
@@ -87,7 +90,7 @@ Partial Class CalculadoraForm
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(98, 243)
+        Me.Button2.Location = New System.Drawing.Point(103, 272)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(80, 80)
         Me.Button2.TabIndex = 0
@@ -96,7 +99,7 @@ Partial Class CalculadoraForm
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(184, 243)
+        Me.Button3.Location = New System.Drawing.Point(189, 272)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(80, 80)
         Me.Button3.TabIndex = 0
@@ -105,16 +108,16 @@ Partial Class CalculadoraForm
         '
         'ButtonMas
         '
-        Me.ButtonMas.Location = New System.Drawing.Point(270, 243)
+        Me.ButtonMas.Location = New System.Drawing.Point(275, 230)
         Me.ButtonMas.Name = "ButtonMas"
-        Me.ButtonMas.Size = New System.Drawing.Size(80, 80)
+        Me.ButtonMas.Size = New System.Drawing.Size(80, 60)
         Me.ButtonMas.TabIndex = 0
         Me.ButtonMas.Text = "+"
         Me.ButtonMas.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(12, 157)
+        Me.Button4.Location = New System.Drawing.Point(17, 186)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(80, 80)
         Me.Button4.TabIndex = 0
@@ -123,7 +126,7 @@ Partial Class CalculadoraForm
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(98, 157)
+        Me.Button5.Location = New System.Drawing.Point(103, 186)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(80, 80)
         Me.Button5.TabIndex = 0
@@ -132,7 +135,7 @@ Partial Class CalculadoraForm
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(184, 157)
+        Me.Button6.Location = New System.Drawing.Point(189, 186)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(80, 80)
         Me.Button6.TabIndex = 0
@@ -141,16 +144,16 @@ Partial Class CalculadoraForm
         '
         'ButtonDividir
         '
-        Me.ButtonDividir.Location = New System.Drawing.Point(270, 157)
+        Me.ButtonDividir.Location = New System.Drawing.Point(275, 165)
         Me.ButtonDividir.Name = "ButtonDividir"
-        Me.ButtonDividir.Size = New System.Drawing.Size(80, 80)
+        Me.ButtonDividir.Size = New System.Drawing.Size(80, 60)
         Me.ButtonDividir.TabIndex = 0
         Me.ButtonDividir.Text = "/"
         Me.ButtonDividir.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(12, 71)
+        Me.Button7.Location = New System.Drawing.Point(17, 100)
         Me.Button7.Name = "Button7"
         Me.Button7.Size = New System.Drawing.Size(80, 80)
         Me.Button7.TabIndex = 0
@@ -159,7 +162,7 @@ Partial Class CalculadoraForm
         '
         'Button8
         '
-        Me.Button8.Location = New System.Drawing.Point(98, 71)
+        Me.Button8.Location = New System.Drawing.Point(103, 100)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(80, 80)
         Me.Button8.TabIndex = 0
@@ -168,7 +171,7 @@ Partial Class CalculadoraForm
         '
         'Button9
         '
-        Me.Button9.Location = New System.Drawing.Point(184, 71)
+        Me.Button9.Location = New System.Drawing.Point(189, 100)
         Me.Button9.Name = "Button9"
         Me.Button9.Size = New System.Drawing.Size(80, 80)
         Me.Button9.TabIndex = 0
@@ -177,9 +180,9 @@ Partial Class CalculadoraForm
         '
         'ButtonMultiplicar
         '
-        Me.ButtonMultiplicar.Location = New System.Drawing.Point(270, 71)
+        Me.ButtonMultiplicar.Location = New System.Drawing.Point(275, 100)
         Me.ButtonMultiplicar.Name = "ButtonMultiplicar"
-        Me.ButtonMultiplicar.Size = New System.Drawing.Size(80, 80)
+        Me.ButtonMultiplicar.Size = New System.Drawing.Size(80, 60)
         Me.ButtonMultiplicar.TabIndex = 0
         Me.ButtonMultiplicar.Text = "X"
         Me.ButtonMultiplicar.UseVisualStyleBackColor = True
@@ -187,22 +190,44 @@ Partial Class CalculadoraForm
         'PantallaTxt
         '
         Me.PantallaTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.PantallaTxt.Location = New System.Drawing.Point(12, 12)
+        Me.PantallaTxt.Location = New System.Drawing.Point(17, 41)
         Me.PantallaTxt.Name = "PantallaTxt"
         Me.PantallaTxt.ReadOnly = True
         Me.PantallaTxt.Size = New System.Drawing.Size(337, 53)
         Me.PantallaTxt.TabIndex = 1
         Me.PantallaTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
+        'PantallaSecundariaTXT
+        '
+        Me.PantallaSecundariaTXT.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.PantallaSecundariaTXT.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PantallaSecundariaTXT.Location = New System.Drawing.Point(17, 12)
+        Me.PantallaSecundariaTXT.Name = "PantallaSecundariaTXT"
+        Me.PantallaSecundariaTXT.ReadOnly = True
+        Me.PantallaSecundariaTXT.Size = New System.Drawing.Size(337, 23)
+        Me.PantallaSecundariaTXT.TabIndex = 1
+        Me.PantallaSecundariaTXT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'ButtonIgual
+        '
+        Me.ButtonIgual.Location = New System.Drawing.Point(275, 362)
+        Me.ButtonIgual.Name = "ButtonIgual"
+        Me.ButtonIgual.Size = New System.Drawing.Size(80, 76)
+        Me.ButtonIgual.TabIndex = 0
+        Me.ButtonIgual.Text = "="
+        Me.ButtonIgual.UseVisualStyleBackColor = True
+        '
         'CalculadoraForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(366, 432)
+        Me.ClientSize = New System.Drawing.Size(366, 466)
+        Me.Controls.Add(Me.PantallaSecundariaTXT)
         Me.Controls.Add(Me.PantallaTxt)
         Me.Controls.Add(Me.ButtonMultiplicar)
         Me.Controls.Add(Me.ButtonDividir)
         Me.Controls.Add(Me.ButtonMas)
+        Me.Controls.Add(Me.ButtonIgual)
         Me.Controls.Add(Me.ButtonMenos)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button6)
@@ -215,7 +240,7 @@ Partial Class CalculadoraForm
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ButtonNull)
+        Me.Controls.Add(Me.ButtonClear)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -226,7 +251,7 @@ Partial Class CalculadoraForm
 
     End Sub
 
-    Friend WithEvents ButtonNull As Button
+    Friend WithEvents ButtonClear As Button
     Friend WithEvents Button0 As Button
     Friend WithEvents ButtonPunto As Button
     Friend WithEvents ButtonMenos As Button
@@ -243,4 +268,6 @@ Partial Class CalculadoraForm
     Friend WithEvents Button9 As Button
     Friend WithEvents ButtonMultiplicar As Button
     Friend WithEvents PantallaTxt As TextBox
+    Friend WithEvents PantallaSecundariaTXT As TextBox
+    Friend WithEvents ButtonIgual As Button
 End Class
--- CalculadoraCientifica/Form1.vb
+++ CalculadoraCientifica/Form1.vb
@@ -2,13 +2,17 @@
     Private parte_entero As String
     Private parte_decimal As String
 
+    Private parte_actual As Integer = 0
+
+    Private operando1 As Decimal
+    Private operando2 As Decimal
+    Private operador As String
+
     Enum PARTE_ACTUAL_OPCIONES
         _ENTERO = 0
         _PUNTO = 1
         _DECIMAL = 2
     End Enum
-
-    Private parte_actual As Integer = 0
 
     Private Sub Button_digits_Click(sender As Object, e As EventArgs) Handles ButtonPunto.Click,
                                                                                Button0.Click,
@@ -51,5 +55,90 @@
                 parte_decimal &= digito
                 PantallaTxt.Text = Val(parte_entero).ToString("#,##0") & "." & parte_decimal
         End Select
+    End Sub
+
+    Private Sub ButtonOperando_Click(sender As Object, e As EventArgs) Handles ButtonMultiplicar.Click,
+                                                                                ButtonDividir.Click,
+                                                                                ButtonMas.Click,
+                                                                                ButtonMenos.Click
+        Try
+            agregar_operacion(sender.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.OkOnly + MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub agregar_operacion(op As Object)
+        If parte_actual = PARTE_ACTUAL_OPCIONES._PUNTO Then
+            PantallaTxt.Text = PantallaTxt.Text.Replace(".", "")
+            parte_actual = PARTE_ACTUAL_OPCIONES._ENTERO
+        End If
+
+        operando1 = Val(PantallaTxt.Text)
+        operador = op
+
+        If parte_actual = PARTE_ACTUAL_OPCIONES._ENTERO Then
+            PantallaSecundariaTXT.Text = String.Format("{0} {1}", operando1.ToString("#,##0"), operador)
+            parte_entero = ""
+            PantallaTxt.Clear()
+        Else
+            PantallaSecundariaTXT.Text = String.Format("{0}.{1} {2}", operando1.ToString("#,##0"), parte_decimal, operador)
+            parte_entero = ""
+            parte_decimal = ""
+            PantallaTxt.Clear()
+        End If
+
+        parte_actual = PARTE_ACTUAL_OPCIONES._ENTERO
+    End Sub
+
+    Private Sub ButtonIgual_Click(sender As Object, e As EventArgs) Handles ButtonIgual.Click
+        Try
+            calcular_resultado()
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOK + vbCritical)
+        End Try
+    End Sub
+
+    Private Sub calcular_resultado()
+        If operador.Length = 0 Then
+            Exit Sub
+        End If
+
+        operando2 = Val(PantallaTxt.Text)
+
+        PantallaTxt.Text = realizar_operacion()
+        PantallaSecundariaTXT.Clear()
+        parte_actual = PARTE_ACTUAL_OPCIONES._ENTERO
+        parte_entero = ""
+        parte_decimal = ""
+    End Sub
+
+    Private Function realizar_operacion() As String
+        Dim resultado As Decimal
+
+        Select Case operador
+            Case "X"
+                resultado = operando1 * operando2
+            Case "/"
+                resultado = operando1 / operando2
+            Case "+"
+                resultado = operando1 + operando2
+            Case "-"
+                resultado = operando1 - operando2
+        End Select
+
+        Return resultado
+    End Function
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        parte_actual = PARTE_ACTUAL_OPCIONES._ENTERO
+        parte_entero = ""
+        parte_decimal = ""
+
+        operando1 = 0
+        operando2 = 0
+        operador = ""
+        PantallaSecundariaTXT.Clear()
+        PantallaTxt.Clear()
     End Sub
 End Class
